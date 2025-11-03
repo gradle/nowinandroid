acfff4c7d607d64daf386d9c194a8ccde57a8893
Vlad Chesnokov <vchesnokov@gradle.com>
2025-11-03 13:40:25 +0100

    Make AGP, buildTools, kotlin and java versions configurable via system properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -89,3 +89,13 @@ check(JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
     https://developer.android.com/build/jdks#jdk-config-in-studio
     """.trimIndent()
 }
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            // Synchronize changes with build-logic/settings.gradle.kts
+            version("androidGradlePlugin", System.getProperty("agpVersion", "8.13.1"))
+            version("kotlin", System.getProperty("kotlinVersion", "2.2.21"))
+        }
+    }
+}
